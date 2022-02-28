53dad45aadc08f6d1105385f866faa74c88be347
HKer <kachunlui4@gmail.com>
2022-02-28 14:09:50 +0000

    enable CORS

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 
+const cors = require('cors');
+
 const path = require('path')
 
 const PORT = process.env.PORT || 3000
 
 
   express()
+  .use(cors())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
