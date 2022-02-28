056ee1069887fb9bb3470bf6c5165c6b4ed7d50d
HKer <kachunlui4@gmail.com>
2022-02-28 13:27:48 +0000

    add bound and hist function

--- index.js
+++ index.js
@@ -11,12 +11,27 @@ const PORT = process.env.PORT || 3000
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/iot',getdata)
+  .get('/iot/hist',gethistory)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+var queue=[];
 
 function getdata(req,res)
 {
-  var now = new Date();
-  var sec=now.getSeconds(); //0-59
+  console.log(req.query)
+  var bound=100;
+  if(req.query!=undefined && req.query.bound!=undefined)
+    max=req.query.bound;
+  var val=Math.floor(Math.random()*(max*2+1))-max;
+  if(queue.length>10)
+    queue.shift();
+  queue.push(val);
+  res.json ({"val":val});
+}
 
-  res.json ({val:sec});
+function gethistory(req,res)
+{
+
+  res.json ({"data":queue});
 }
