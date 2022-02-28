a4511461f249ab65a0352fc61eee4d5f5299abf4
HKer <kachunlui4@gmail.com>
2022-02-28 13:04:38 +0000

    add iot api return server seconds

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 
-express()
+const path = require('path')
+
+const PORT = process.env.PORT || 3000
+
+
+  express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/iot',getdata)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function getdata(req,res)
+{
+  var now = new Date();
+  var sec=now.getSeconds(); //0-59
+
+  res.json ({val:sec});
+}
